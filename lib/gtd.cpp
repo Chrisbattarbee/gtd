7bd0ba199a7f65881da7ff880d8ae80bd7dcf600
Chris Battarbee <chris@chrisbattarbee.com>
2020-02-10 14:17:32 +0000

    Add initial GTD interface

---
+++ lib/gtd.cpp
@@ -0,0 +1,44 @@
+//
+// Created by chris on 10/02/2020.
+//
+
+#include "gtd.h"
+#include <filesystem>
+
+
+Project::Project(std::string path) {
+    this->name = name;
+    this->backing_org_file = new OrgModeFile(path);
+}
+
+void Project::add_item(std::string title, std::optional<TodoStatus> todo_status) {
+    this->add_sub_item(this->backing_org_file->get_root_item(), title, todo_status);
+}
+
+void Project::add_sub_item(Item *parent_item, std::string title, std::optional<TodoStatus> todo_status) {
+    Item *new_item = todo_status != std::nullopt ?
+                     new Item(this->backing_org_file->get_root_item(), title, todo_status.value()) :
+                     new Item(this->backing_org_file->get_root_item(), title);
+
+    parent_item->add_child(new_item);
+}
+
+Item *Project::get_root_item() {
+    return this->backing_org_file->get_root_item();
+}
+
+Gtd::Gtd(std::string root_directory) {
+    this->root_directory = root_directory;
+    this->projects = new std::vector<Project*>;
+
+    // Read all existing projects in
+    for (const auto & entry : std::filesystem::directory_iterator(root_directory)) {
+        auto* project = new Project(entry.path());
+        this->projects->push_back(project);
+    }
+}
+
+Project *Gtd::create_project(std::string name) {
+    auto* project = new Project(root_directory + name);
+    this->projects->push_back(project);
+}
---
+++ lib/gtd.h
@@ -0,0 +1,32 @@
+//
+// Created by chris on 10/02/2020.
+//
+
+#ifndef GTD_GTD_H
+#define GTD_GTD_H
+
+#include "org_mode_file.h"
+
+class Project {
+public:
+    Project(std::string path);
+    void add_item(std::string title, std::optional<TodoStatus> todo_status);
+    void add_sub_item(Item* parent_item, std::string title, std::optional<TodoStatus> todo_status);
+    Item* get_root_item();
+private:
+    OrgModeFile* backing_org_file;
+    std::string name;
+};
+
+class Gtd {
+public:
+    Gtd(std::string root_directory);
+    Project* create_project(std::string name);
+    Project* get_project(std::string name);
+private:
+    std::string root_directory;
+    std::vector<Project*>* projects;
+};
+
+
+#endif //GTD_GTD_H
